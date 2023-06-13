ad6d70f6d6a2d4ee1c392e06b09ede04a365560e
Jakub Urbaniak <jakuburbaniak@Jakubs-MacBook-Pro.local>
2023-06-13 14:13:53 +0200

    fix:Changed TypeOrm to Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [],
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
