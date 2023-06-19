4d6daf992d575d89b35f530f333614b0f1f18ec2
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-19 12:32:08 +0200

    refactor: remove Jwt from recipe module

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Module({
-  providers: [RecipeService, PrismaService, JwtStrategy, JwtAuthGuard],
+  providers: [RecipeService, PrismaService, JwtAuthGuard],
   controllers: [RecipeController],
   exports: [RecipeService],
 })
