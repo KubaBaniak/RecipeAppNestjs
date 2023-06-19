256dd9c7a33e01ff4025b9bd9e00e8687311b941
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-19 11:16:16 +0200

    refactor: refactor jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   validate(payload: JwtPayload): JwtPayload {
-    console.log(payload);
-    return { userId: payload.userId };
+    return { userId: payload.userId};
   }
 }
