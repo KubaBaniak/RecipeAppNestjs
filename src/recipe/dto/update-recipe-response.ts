5df011179de71016d5d7102b2a9b59ca81e41556
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-20 15:21:49 +0200

    refactor: refactor recipe update response

--- src/recipe/dto/update-recipe-response.ts
+++ src/recipe/dto/update-recipe-response.ts
@@ -1,14 +1,23 @@
-interface IUpdatedRecipe {
-  title: string;
-  description: string;
-  ingredients: string;
-  preparation: string;
-}
+import { Recipe } from '@prisma/client';
 
 export class UpdatedRecipeResponse {
-  constructor(public updatedRecipes: IUpdatedRecipe) {}
+  constructor(
+    public id: number,
+    public createdAt: Date,
+    public title: string,
+    public description: string,
+    public ingredients: string,
+    public preparation: string,
+  ) {}
 
-  public static from(updatedRecipes: IUpdatedRecipe): UpdatedRecipeResponse {
-    return new UpdatedRecipeResponse(updatedRecipes);
+  public static from(updatedRecipe: Recipe): UpdatedRecipeResponse {
+    return new UpdatedRecipeResponse(
+      updatedRecipe.id,
+      updatedRecipe.createdAt,
+      updatedRecipe.title,
+      updatedRecipe.description,
+      updatedRecipe.ingredients,
+      updatedRecipe.preparation,
+    );
   }
 }
