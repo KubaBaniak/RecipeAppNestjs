573f8bfe80d54f7be3022beae0a3d9cef0f960de
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-21 10:31:38 +0200

    feat: add documentation for all requests, and auth responses

--- src/auth/dto/sign-up-response.ts
+++ src/auth/dto/sign-up-response.ts
@@ -1,9 +1,19 @@
 import { User } from '@prisma/client';
 
 export class SignUpResponse {
-  constructor(public user: User) {}
+  public id: number;
+  public email: string;
+  public password: string;
+  public role: string;
+
+  constructor(id: number, email: string, password: string, role: string) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
 
   public static from(user: User): SignUpResponse {
-    return new SignUpResponse(user);
+    return new SignUpResponse(user.id, user.email, user.password, user.role);
   }
 }
