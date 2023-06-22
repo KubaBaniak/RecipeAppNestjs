71e09a57a16aa2cf4d4bae5465efd77c7a545454
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-22 14:03:56 +0200

    refactor: refactor sign up endpoint so it does not return hashed user password

--- src/auth/dto/sign-up-response.ts
+++ src/auth/dto/sign-up-response.ts
@@ -1,14 +1,9 @@
 import { User } from '@prisma/client';
 
 export class SignUpResponse {
-  constructor(
-    public id: number,
-    public email: string,
-    public password: string,
-    public role: string,
-  ) {}
+  constructor(public id: number, public email: string, public role: string) {}
 
   public static from(user: User): SignUpResponse {
-    return new SignUpResponse(user.id, user.email, user.password, user.role);
+    return new SignUpResponse(user.id, user.email, user.role);
   }
 }
