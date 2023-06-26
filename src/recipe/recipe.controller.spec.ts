3822aa5e76bace64efcb3c274dbd066cdfa49091
Jakub Urbaniak <kuba121201@gmail.com>
2023-06-26 14:17:27 +0200

    refactor: apply review suggestions

--- src/recipe/recipe.controller.spec.ts
+++ src/recipe/recipe.controller.spec.ts
@@ -2,174 +2,172 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { faker } from '@faker-js/faker';
 
 describe("Recipe Controller's tests", () => {
-    let recipeController: RecipeController;
-    let recipeService: RecipeService;
-    let prismaService: PrismaService;
+  let recipeController: RecipeController;
+  let recipeService: RecipeService;
+  let prismaService: PrismaService;
 
-    const mockRecipe = {
-        id: expect.any(Number),
-        createdAt: expect.any(Number),
-        title: expect.any(String),
-        description: expect.any(String),
-        ingredients: expect.any(String),
-        preparation: expect.any(String),
-    };
+  const mockRecipe = {
+    id: expect.any(Number),
+    createdAt: expect.any(Number),
+    title: expect.any(String),
+    description: expect.any(String),
+    ingredients: expect.any(String),
+    preparation: expect.any(String),
+  };
 
-    class mockRecipeRequest {
-        title: string;
-        description: string;
-        ingredients: string;
-        preparation: string;
-    }
+  class mockRecipeRequest {
+    title: string;
+    description: string;
+    ingredients: string;
+    preparation: string;
+  }
 
-    const mockRecipeService = {
-        createRecipe: jest.fn((dto: mockRecipeRequest) => {
-            return {
-                id: 1,
-                createdAt: Date.now(),
-                ...dto,
-            };
-        }),
-        fetchRecipe: jest.fn((id: number) => {
-            return {
-                id,
-                createdAt: Date.now(),
-                title: 'Fetched recipe title',
-                description: 'Fetched recipe description',
-                ingredients: 'Fetched recipe ingredients',
-                preparation: 'Fetched recipe preparation',
-            };
-        }),
-        fetchAllRecipes: jest.fn(() => {
-            return [
-                {
-                    id: 0,
-                    createdAt: Date.now(),
-                    title: 'Fetched recipe title',
-                    description: 'Fetched recipe description',
-                    ingredients: 'Fetched recipe ingredients',
-                    preparation: 'Fetched recipe preparation',
-                },
-                {
-                    id: 1,
-                    createdAt: Date.now(),
-                    title: 'Fetched recipe title',
-                    description: 'Fetched recipe description',
-                    ingredients: 'Fetched recipe ingredients',
-                    preparation: 'Fetched recipe preparation',
-                },
-            ];
-        }),
-        updateRecipe: jest.fn((id: number, dto: mockRecipeRequest) => {
-            return {
-                id,
-                createdAt: Date.now(),
-                title: dto.title || 'latest title',
-                description: dto.description || 'latest description',
-                ingredients: dto.ingredients || 'latest ingredients',
-                preparation: dto.preparation || 'latest preparation',
-            };
-        }),
-        deleteRecipe: jest.fn((_id: number) => {
-            return Promise<void>
-        })
-    };
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [RecipeController],
-            providers: [
-                {
-                    provide: RecipeService,
-                    useValue: mockRecipeService,
-                },
-                PrismaService,
-            ],
-        }).compile();
+  const mockRecipeService = {
+    createRecipe: jest.fn((dto: mockRecipeRequest) => {
+      return {
+        id: 1,
+        createdAt: Date.now(),
+        ...dto,
+      };
+    }),
+    fetchRecipe: jest.fn((id: number) => {
+      return {
+        id,
+        createdAt: Date.now(),
+        title: faker.word.noun(),
+        description: faker.lorem.text(),
+        ingredients: faker.lorem.words(4),
+        preparation: faker.lorem.lines(5),
+      };
+    }),
+    fetchAllRecipes: jest.fn(() => {
+      return [
+        {
+          id: 0,
+          createdAt: Date.now(),
+          title: faker.word.noun(),
+          description: faker.lorem.text(),
+          ingredients: faker.lorem.words(4),
+          preparation: faker.lorem.lines(5),
+        },
+        {
+          id: 1,
+          createdAt: Date.now(),
+          title: faker.word.noun(),
+          description: faker.lorem.text(),
+          ingredients: faker.lorem.words(4),
+          preparation: faker.lorem.lines(5),
+        },
+      ];
+    }),
+    updateRecipe: jest.fn((id: number, dto: mockRecipeRequest) => {
+      return Promise.resolve({
+        id,
+        createdAt: Date.now(),
+        title: dto.title || faker.word.noun(),
+        description: dto.description || faker.lorem.text(),
+        ingredients: dto.ingredients || faker.lorem.words(4),
+        preparation: dto.preparation || faker.lorem.lines(5),
+      });
+    }),
+    deleteRecipe: jest.fn((id: number) => Promise.resolve()),
+  };
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RecipeController],
+      providers: [
+        {
+          provide: RecipeService,
+          useValue: mockRecipeService,
+        },
+        PrismaService,
+      ],
+    }).compile();
 
-        prismaService = module.get<PrismaService>(PrismaService);
-        recipeService = module.get<RecipeService>(RecipeService);
-        recipeController = module.get<RecipeController>(RecipeController);
+    prismaService = module.get<PrismaService>(PrismaService);
+    recipeService = module.get<RecipeService>(RecipeService);
+    recipeController = module.get<RecipeController>(RecipeController);
+  });
+
+  it('should be defined', () => {
+    expect(recipeController).toBeDefined();
+  });
+
+  describe('CreateRecipe', () => {
+    it('should create recipe', async () => {
+      //given
+      const request = {
+        title: faker.word.noun(),
+        description: faker.lorem.text(),
+        ingredients: faker.lorem.words(4),
+        preparation: faker.lorem.lines(5),
+      };
+
+      //when
+      const createdRecipe = await recipeController.createRecipe(request);
+
+      //then
+      expect(createdRecipe).toEqual({
+        ...mockRecipe,
+      });
     });
+  });
+  describe('FetchRecipe', () => {
+    it('should fetch recipe given id', async () => {
+      //given
+      const id = 1;
 
-    it('should be defined', () => {
-        expect(recipeController).toBeDefined();
+      //when
+      const fetchedRecipe = await recipeController.fetchRecipe(id);
+
+      //then
+      expect(fetchedRecipe).toEqual({
+        fetchedRecipe: {
+          ...mockRecipe,
+        },
+      });
     });
+    it('should fetch all recipes', async () => {
+      //when
+      const fetchedAllRecipes = await recipeController.fetchRecipes();
 
-    describe('CreateRecipe', () => {
-        it('should create recipe', async () => {
-            //given
-            const dto = {
-                title: 'fries',
-                description: '>>> mcdonalds',
-                ingredients: 'potatoes',
-                preparation: 'peel and fry',
-            };
-
-            //when
-            const createdRecipe = await recipeController.createRecipe(dto);
-
-            //then
-            expect(createdRecipe).toEqual({
-                ...mockRecipe,
-            });
-        });
+      //then
+      expect(fetchedAllRecipes.fetchedRecipes).toEqual(
+        expect.arrayContaining([mockRecipe]),
+      );
     });
-    describe('FetchRecipe', () => {
-        it('should fetch recipe given id', async () => {
-            //given
-            const id = '1';
+  });
+  describe('UpdateRecipe', () => {
+    it('should update recipe with given credentials', async () => {
+      //given
+      const id = 1;
+      const request = {
+        title: faker.word.noun(),
+        description: faker.lorem.text(),
+        ingredients: faker.lorem.words(4),
+        preparation: faker.lorem.lines(5),
+      };
 
-            //when
-            const fetchedRecipe = await recipeController.fetchRecipe(+id);
+      //when
+      const updatedRecipe = await recipeController.updateRecipe(id, request);
 
-            //then
-            expect(fetchedRecipe).toEqual({
-                fetchedRecipe: {
-                    ...mockRecipe,
-                },
-            });
-        });
-        it('should fetch all recipes', async () => {
-            //when
-            const fetchedAllRecipes = await recipeController.fetchRecipes();
-
-            //then
-            expect(fetchedAllRecipes.fetchedRecipes).toEqual(
-                expect.arrayContaining([mockRecipe]),
-            );
-        });
+      //then
+      expect(updatedRecipe).toEqual(mockRecipe);
     });
-    describe('UpdateRecipe', () => {
-        it('should update recipe with given credentials', async () => {
-            //given
-            const id = '1';
-            const dto = {
-                title: 'updated title',
-                description: 'updated description',
-                ingredients: 'updated ingredients',
-                preparation: 'updated preparation',
-            };
+  });
+  describe('DeleteRecipe', () => {
+    it('should delete recipe with given id', async () => {
+      //given
+      const id = 1;
 
-            //when
-            const updatedRecipe = await recipeController.updateRecipe(+id, dto);
+      //when
+      const deletedRecipe = await recipeController.deleteRecipe(id);
 
-            //then
-            expect(updatedRecipe).toEqual(mockRecipe);
-        });
+      //then
+      expect(deletedRecipe).toBeUndefined();
     });
-    describe('DeleteRecipe', () => {
-        it('should delete recipe with given id', async () => {
-            //given
-            const id = '1';
-
-            //when
-            const deletedRecipe = await recipeController.deleteRecipe(+id);
-            console.log(deletedRecipe);
-
-            //then
-            expect(deletedRecipe).toBeUndefined();
-        });
-    });
+  });
 });
