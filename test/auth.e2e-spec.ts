b0459045edf02d9c439f9d56c2986efcc61dc7c6
Jakub Urbaniak <kuba121201@gmail.com>
2023-07-04 16:01:31 +0200

    refactor: clear db after each test, and create indepented user for each one

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -11,7 +11,8 @@ import { userData } from './mock.user';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
-  const mockUserRequest = userData;
+  let prismaService: PrismaService;
+  let authService: AuthService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -20,7 +21,13 @@ describe('AuthController (e2e)', () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
+    prismaService = moduleRef.get<PrismaService>(PrismaService);
+    authService = moduleRef.get<AuthService>(AuthService);
     await app.init();
+  });
+
+  afterEach(async () => {
+    await prismaService.user.deleteMany();
   });
 
   describe('/auth/signup (POST)', () => {
@@ -28,30 +35,32 @@ describe('AuthController (e2e)', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .set('Accept', 'application/json')
-        .send(mockUserRequest)
+        .send(userData)
         .expect((response: request.Response) => {
           const { id, email, role } = response.body;
           expect(id).toEqual(expect.any(Number));
-          expect(email).toEqual(mockUserRequest.email);
+          expect(email).toEqual(userData.email);
           expect(role).toEqual(Role.USER);
         })
         .expect(HttpStatus.CREATED);
     });
-    it('should not register a user (already in db) and return 403 error (FORBIDDEN ACCESS)', () => {
+    it('should not register a user (already in db) and return 403 error (FORBIDDEN ACCESS)', async () => {
+      await prismaService.user.create({ data: userData });
       return request(app.getHttpServer())
         .post('/auth/signup')
         .set('Accept', 'application/json')
-        .send(mockUserRequest)
+        .send(userData)
         .expect(HttpStatus.FORBIDDEN);
     });
   });
 
   describe('/auth/signin (POST)', () => {
     it('should generate access token for user', async () => {
+      await authService.signUp(userData);
       return request(app.getHttpServer())
         .post('/auth/signin')
         .set('Accept', 'application/json')
-        .send(mockUserRequest)
+        .send(userData)
         .expect((response: request.Response) => {
           const accessToken = response.body.accessToken;
           expect(accessToken).toEqual(expect.any(String));
@@ -60,23 +69,25 @@ describe('AuthController (e2e)', () => {
     });
 
     it('should not generate access token for user (wrong email) and return 401 error (UNAUTHORIZED)', async () => {
+      await authService.signUp(userData);
       return request(app.getHttpServer())
         .post('/auth/signin')
         .set('Accept', 'application/json')
         .send({
-          email: mockUserRequest.email,
-          password: mockUserRequest.password + faker.word.noun(),
+          email: userData.email,
+          password: userData.password + faker.word.noun(),
         })
         .expect(HttpStatus.UNAUTHORIZED);
     });
 
-    it('should not generate access token for user (wrong password) and return 401 error (UNAUTHORIZED)', () => {
+    it('should not generate access token for user (wrong password) and return 401 error (UNAUTHORIZED)', async () => {
+      await prismaService.user.create({ data: userData });
       return request(app.getHttpServer())
         .post('/auth/signin')
         .set('Accept', 'application/json')
         .send({
-          email: mockUserRequest.email,
-          password: mockUserRequest.password + faker.word.noun(),
+          email: userData.email,
+          password: userData.password + faker.word.noun(),
         })
         .expect(HttpStatus.UNAUTHORIZED);
     });
