02ab359edf589f8d21a740d4d8c05a37abc7e73d
Jakub Urbaniak <kuba121201@gmail.com>
2023-07-06 11:54:05 +0200

    refactor: apply review suggestions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController).toBeDefined();
-    });
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
