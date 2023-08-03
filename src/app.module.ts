e62177562ac68f440c9a08c729a84f7d01807bdc
Jakub Urbaniak <kuba121201@gmail.com>
2023-08-03 15:05:15 +0200

    chore: basic files / receiving a message

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisCacheModule } from './cache/redis-cache.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { RedisCacheModule } from './cache/redis-cache.module';
     RedisCacheModule,
     RecipeModule,
     PrismaModule,
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
