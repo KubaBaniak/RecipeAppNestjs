14c0d0bfe715d90360dca6795086c810dc74ddde
Jakub Urbaniak <kuba121201@gmail.com>
2023-08-09 11:12:00 +0200

    fix: validation and add documentation

--- src/recipe/dto/update-recipe-request.ts
+++ src/recipe/dto/update-recipe-request.ts
@@ -1,21 +1,31 @@
-import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateRecipeRequest {
   @IsNotEmpty()
   @IsOptional()
+  @IsString()
+  @ApiProperty()
   title?: string;
 
   @IsOptional()
+  @IsString()
+  @ApiProperty()
   description?: string;
 
   @IsOptional()
   @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
   ingredients?: string;
 
   @IsOptional()
+  @IsString()
+  @ApiProperty()
   preparation?: string;
 
   @IsOptional()
   @IsBoolean()
+  @ApiProperty()
   isPublic?: boolean;
 }
