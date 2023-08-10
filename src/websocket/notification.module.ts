2c3c080f9f66f054ea1bb13e863ae74946ff6d1e
Jakub Urbaniak <kuba121201@gmail.com>
2023-08-10 15:54:32 +0200

    fix: add providers to notification module to fix test

--- src/websocket/notification.module.ts
+++ src/websocket/notification.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationGateway } from './notification.gateway';
+import { AuthService } from '../auth/auth.service';
+import { UserRepository } from '../user/user.repository';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [NotificationGateway],
+  providers: [NotificationGateway, UserRepository, AuthService, PrismaService],
   exports: [NotificationGateway],
 })
 export class NotificationModule {}
