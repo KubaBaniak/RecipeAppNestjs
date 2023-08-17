b5c28c562bc7817c61a106da840283845746bf4e
Jakub Urbaniak <kuba121201@gmail.com>
2023-08-17 12:21:10 +0200

    chore: add encryption for tokens

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -8,6 +8,7 @@ import {
 import { CreateWebhookRequest, ListWebhooksDto } from './dto';
 import { PATRepository } from './webhook.repository';
 import { Recipe } from '@prisma/client';
+import Cryptr from 'cryptr';
 
 @Injectable()
 export class WebhookService {
@@ -15,6 +16,7 @@ export class WebhookService {
     private readonly httpService: HttpService,
     private readonly patRepository: PATRepository,
   ) {}
+  cryptr = new Cryptr(process.env.CRYPTR_SECRET_KEY);
 
   async createWebhook(
     userId: number,
@@ -25,6 +27,11 @@ export class WebhookService {
     );
     if (userWebhooks.length >= 5) {
       throw new ForbiddenException();
+    }
+
+    if (webhookData.token) {
+      const encryptedString = this.cryptr.encrypt(webhookData.token);
+      webhookData.token = encryptedString;
     }
 
     this.patRepository.createWebhook(userId, webhookData);
@@ -45,7 +52,11 @@ export class WebhookService {
     return this.patRepository.getAllWebhooksByUserId(userId);
   }
 
-  sendToWebhook(url: string, data: any, token?: string): void {
+  sendToWebhook(url: string, data: Recipe, token?: string): void {
+    if (token) {
+      token = this.cryptr.decrypt(token);
+    }
+
     const headersRequest = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
