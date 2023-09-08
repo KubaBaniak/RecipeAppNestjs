ae30624e9df0f190ef7b79791e9fdc6fc7ed182c
Jakub Urbaniak <kuba121201@gmail.com>
2023-09-08 13:51:06 +0200

    refactor: refactor user factory and mocked auth service

--- src/auth/__mocks__/auth.service.mock.ts
+++ src/auth/__mocks__/auth.service.mock.ts
@@ -1,6 +1,8 @@
 import { SignInRequest, SignUpRequest, UserRequest } from '../dto';
 import { faker } from '@faker-js/faker';
-import { User, Role } from '@prisma/client';
+import { User } from '@prisma/client';
+import { createUserResponse } from 'src/user/test/user.factory';
+import qrcode from 'qrcode';
 
 export class MockAuthService {
   signIn(_signInRequest: SignInRequest): Promise<string> {
@@ -8,28 +10,55 @@ export class MockAuthService {
   }
 
   signUp(signUpRequest: SignUpRequest): Promise<User> {
-    return Promise.resolve({
-      id: faker.number.int(),
-      email: signUpRequest.email,
-      password: signUpRequest.password,
-      role: Role.USER,
-      enabled2FA: false,
-      recoveryKeys: [],
-    });
+    return Promise.resolve(
+      createUserResponse({
+        email: signUpRequest.email,
+        password: signUpRequest.password,
+      }),
+    );
   }
 
-  validateUser(userRequest: UserRequest): Promise<User> {
-    return Promise.resolve({
-      id: faker.number.int(),
-      email: userRequest.email,
-      password: userRequest.password,
-      role: Role.USER,
-      enabled2FA: false,
-      recoveryKeys: [],
-    });
+  validateUser(_userRequest: UserRequest): Promise<User> {
+    return Promise.resolve(createUserResponse());
   }
 
   changePassword(_userId: number, _newPassword: string): Promise<void> {
     return Promise.resolve();
+  }
+
+  async successfullLoginToken(_id: number, _email: string): Promise<string> {
+    return Promise.resolve(faker.string.sample(64));
+  }
+
+  async createQrcodeFor2FA(_userId: number): Promise<string> {
+    const data = {
+      email: faker.internet.email(),
+      service: faker.word.noun(),
+    };
+
+    return qrcode.toDataURL(JSON.stringify(data));
+  }
+
+  async enable2FA(_userId: number, _providedToken: string): Promise<string[]> {
+    return Promise.resolve(
+      Array.from({ length: 3 }, () => faker.string.alphanumeric(16)),
+    );
+  }
+
+  async disable2FA(userId: number, _providedToken: string): Promise<User> {
+    return Promise.resolve(
+      createUserResponse({ id: userId, enabled2FA: true }),
+    );
+  }
+
+  async verify2FA(_userId: number, _token: string): Promise<string> {
+    return Promise.resolve(faker.string.sample(64));
+  }
+
+  async recoverAccountWith2FA(
+    _userId: number,
+    _providedRecoveryKey: string,
+  ): Promise<string> {
+    return Promise.resolve(faker.string.sample(64));
   }
 }
--- src/user/test/user.factory.ts
+++ src/user/test/user.factory.ts
@@ -1,11 +1,32 @@
 import { faker } from '@faker-js/faker';
+import { Role } from '@prisma/client';
 
 type CreateUserOverrides = {
   email?: string;
   password?: string;
 };
 
+type CreateUserResponseOverrides = {
+  id?: number;
+  email?: string;
+  password?: string;
+  role?: Role;
+  enabled2FA?: boolean;
+  recoveryKeys?: string[];
+};
+
 export const createUser = (overrides: CreateUserOverrides = {}) => ({
   email: overrides.email ?? faker.internet.email(),
   password: overrides.password ?? faker.internet.password(),
+});
+
+export const createUserResponse = (
+  overrides: CreateUserResponseOverrides = {},
+) => ({
+  id: overrides.id ?? faker.number.int(),
+  email: overrides.email ?? faker.internet.email(),
+  password: overrides.password ?? faker.internet.password(),
+  role: overrides.role ?? Role.USER,
+  enabled2FA: overrides.enabled2FA ?? false,
+  recoveryKeys: overrides.recoveryKeys ?? [],
 });
