f50e7ef5af5a555fdcda462204c67a33fe71b821
Jakub Urbaniak <kuba121201@gmail.com>
2023-09-08 10:06:36 +0200

    refactor: add dto validation in change password controller test

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -4,6 +4,9 @@ import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
 import { MockAuthService } from '../__mocks__/auth.service.mock';
 import { Role } from '@prisma/client';
+import { ChangePasswordRequest } from '../dto';
+import { validate } from 'class-validator';
+import { plainToClassFromExist, plainToInstance } from 'class-transformer';
 
 describe('AuthController', () => {
   let authController: AuthController;
@@ -67,16 +70,22 @@ describe('AuthController', () => {
   describe('Change password', () => {
     it('should change password', async () => {
       //given
-      const request = {
+      const request: ChangePasswordRequest = {
         newPassword: faker.internet.password(),
       };
       const userId = faker.number.int();
-      const spy = jest.spyOn(authService, 'changePassword');
+      jest.spyOn(authService, 'changePassword');
+      const changePasswordRequestDto = plainToInstance(
+        ChangePasswordRequest,
+        request,
+      );
 
       //when
+      const errors = await validate(changePasswordRequestDto);
       await authController.changePassword(userId, request);
 
       //then
+      expect(errors).toHaveLength(0);
       expect(authService.changePassword).toBeCalled();
     });
   });
